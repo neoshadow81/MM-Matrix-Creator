6aea3fe280061a6945736c8ca0962b45b6fd1cb5
Marc Delpont <marc@neoshadow.net>
2020-07-24 10:07:52 +0200

    First commit

---
+++ MMM-Matrix-Creator.js
@@ -0,0 +1,20 @@
+Module.register('MMM-Matrix-Creator', {
+
+	defaults: {
+		
+	},
+
+	getDom: function() {
+		var wrapper = document.createElement("div")
+		wrapper.innerHTML = ''
+		return wrapper
+	}
+    
+    resume: function() {
+        var self = this;
+    }
+    
+    suspend() {
+        
+    }
+})
---
+++ README.md
@@ -0,0 +1,2 @@
+# MM-Matrix-Creator
+MagicMirror² module for managing Matrix Creator IoT development board
